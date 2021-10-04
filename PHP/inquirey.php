d25677777a13cf93c42f5b18f4131a64525887c5
wiltegic <58319720+wiltegic@users.noreply.github.com>
2021-10-04 19:41:44 +0530

    Commit For Backend

---
+++ PHP/inquirey.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+    <head> 
+        <!-- basic -->         
+        <meta charset="utf-8"> 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+        <!-- mobile metas -->         
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1"> 
+        <!-- site metas -->         
+        <title>The Yellow Cabs</title>         
+        <meta name="keywords" content=""> 
+        <meta name="description" content=""> 
+        <meta name="author" content=""> 
+        <!-- bootstrap css -->         
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"> 
+        <!-- style css -->         
+        <link rel="stylesheet" href="css/style.css"> 
+        <!-- Responsive-->         
+        <link rel="stylesheet" href="css/responsive.css"> 
+        <!-- fevicon -->         
+        <link rel="icon" href="images/fevicon.png" type="image/gif"/> 
+        <!-- Scrollbar Custom CSS -->         
+        <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css"> 
+        <!-- Tweaks for older IEs-->         
+        <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css"> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen"> 
+        <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->         
+        <link rel="stylesheet" href="components/pg.blocks/css/blocks.css"> 
+        <link rel="stylesheet" href="components/pg.blocks/css/plugins.css"> 
+        <link rel="stylesheet" href="components/pg.blocks/css/style-library-1.css"> 
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700"> 
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic"> 
+        <link rel="stylesheet" href="blocks.css"> 
+        <link rel="stylesheet" href="components/pg.blocks.wp/css/blocks.css">
+        <link rel="stylesheet" href="components/pg.blocks.wp/css/plugins.css">
+        <link rel="stylesheet" href="components/pg.blocks.wp/css/style-library-1.css">
+    </head>     
+    <!-- body -->     
+    <body class="main-layout"> 
+        <!-- loader  -->         
+        <div class="loader_bg"> 
+            <div class="loader"> 
+                <img src="images/loading.gif" alt="#" style="width: 150px;"/> 
+            </div>             
+        </div>         
+        <!-- end loader -->         
+        <!-- header -->         
+        <header> 
+            <!-- header inner -->             
+            <div class="header pt-3"> 
+                <div class="row"> 
+                    <div class="col-6 col-md-4 mr-0 pl-4 pr-0 d-flex flex-wrap align-items-center">
+                        <a href="index.php"><img src="images/logo.png" style="width: 250px;" alt="The Yellow Cabs"></a> 
+                    </div>
+                    <div class="col-md-4 col-6">
+                        <div class="ml-0 mr-0 pl-0 pr-0 sign_btn text-center" style="white-space: nowrap;">
+                            <a href="tel:+919638802666" class="pl-2 pr-2" style="font-size: 125%;"><i class="fa fa-lg fa-phone"></i>+91-9638802666</a>
+                        </div>                         
+                    </div>
+                    <div class="col-md-4">
+                        <div class="d-none d-sm-block pl-0 sign_btn text-right text-sm-right">
+                            <a href="inquirey.php" class="pl-3 pr-3" style="font-size: 120%;">Inquirey</a>
+                        </div>                         
+                    </div>
+                </div>                 
+            </div>             
+        </div>         
+        <img src="images/student-meeting-teacher.jpg" width="100%"/> 
+        <section class="bg-light pb-5 pt-5 text-center"> 
+            <div class="container pb-4 pt-4"> 
+                <div class="row"> 
+                    <div class="col-lg-8 ml-auto mr-auto py-3"> 
+                        <h2 class="font-weight-bold text-uppercase">Inquirey</h2> 
+                        <p class="mb-5 text-secondary">Enter Your Details Below</p> 
+                        <form> 
+                            <div class="form-group"> 
+                                <input type="text" class="border border-1 form-control rounded-1" placeholder="Full Name" id="name" name="name"> 
+                            </div>                             
+                            <div class="form-group"> 
+                                <input type="tel" class="border border-1 form-control rounded-1" placeholder="Contact Number" id="mobile_number" name="mobile_number"> 
+                            </div>                             
+                            <div class="form-group"> 
+                                <input type="email" class="border border-1 form-control rounded-1" placeholder="Email Address" id="email" name="email"> 
+                            </div>                             
+                            <div class="form-group"> 
+                                <textarea class="border-1 form-control rounded-1" rows="6" placeholder="Enter your Inquirey..." id="inquirey" name="inquirey"></textarea> 
+                            </div>                             
+                            <div class="form-group"> 
+</div>                             
+                            <button type="submit" class="btn btn-block btn-warning rounded-1 text-body text-uppercase" id="submit" name="submit">Submit</button>                             
+                        </form>                         
+                    </div>                     
+                </div>                 
+            </div>             
+        </section>         
+    </header>     
+    <!-- end header inner -->     
+    <!-- end header -->     
+    <!-- banner -->     
+    <!-- end banner -->     
+    <!-- form_lebal -->     
+    <section> 
+        <div class="container"> 
+            <div class="row"> 
+                <div class="col-md-12"> 
+</div>                 
+            </div>             
+        </div>         
+    </section>     
+    <!-- end form_lebal -->     
+    <!-- choose  section -->     
+    <!-- end choose  section -->     
+    <!-- our  section -->     
+    <!-- end our  section -->     
+    <!-- about -->     
+    <!-- end about -->     
+    <!-- testimonial -->     
+    <section>
+        <section class="bg-dark content-block-nopad footer-wrap-1-1">
+            <div class="container footer-1-1">
+                <div class="row">
+                    <div class="col-sm-6 col-sm-offset-1">
+                        <div class="row">
+</div>
+                    </div>
+                </div>
+                <!-- /.row -->
+                <div class="row">
+                    <div class="col-md-4 col-xl-4">
+                        <h4 class="text-white">COMPANY</h4>
+                        <ul>
+                            <li style="border:rgba(0, 0, 0, 0.19);">
+                                <a href="about.php" class="text-white">● About us</a>
+                            </li>
+                            <li style="border:rgba(0, 0, 0, 0.19);">
+                                <a href="privacy.php" class="text-white">● Privacy Policy</a>
+                            </li>
+                            <li style="border:rgba(0, 0, 0, 0.19);">
+                                <a href="terms.php" class="text-white">● Terms &amp; Conditions</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 col-xl-4">
+                        <h4 class="text-white">SERVICES&nbsp;</h4>
+                        <ul>
+                            <li style="border:rgba(0, 0, 0, 0.19);">
+                                <a href="index.php" class="text-white">● Local Car Rentals</a>
+                            </li>
+                            <li style="border:rgba(0, 0, 0, 0.19);">
+                                <a href="index.php" class="text-white">● Outstation Taxi</a>
+                            </li>
+                            <li style="border:rgba(0, 0, 0, 0.19);">
+                                <a href="attachtaxi.php" class="text-white">● Attach Taxi</a>
+                            </li>
+                            <li style="border:rgba(0, 0, 0, 0.19);">
+                                <a href="corporate.php" class="text-white">● Corporate Car Rental</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 col-xl-4">
+                        <h4 class="text-white">GET IN TOUCH&nbsp;</h4>
+                        <ul>
+                            <li style="border:rgba(0, 0, 0, 0.19);">
+                                <a href="contact.php" class="text-white">● Contact Us</a>
+                            </li>
+                            <li style="border:rgba(0, 0, 0, 0.19);">
+                                <span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="bg-primary fa fa-facebook-square fa-inverse fa-stack-1x" style="border-radius: 25px;"></i></span>
+                                <span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="bg-info fa fa-inverse fa-stack-1x fa-twitter-square" style="border-radius: 25px;"></i></span>
+                                <span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="bg-danger fa fa-instagram fa-inverse fa-stack-1x" style="border-radius: 25px;"></i></span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- /.container -->
+        </section>
+        <div class="copyright-bar" style="background-color: #FDB813;">
+            <div class="container">
+                <p class="text-body text-center">Copyright &copy; 2021  All Right Reserved | <a href="" class="text-body">WILTEGIC</a></p>
+            </div>
+        </div>
+    </section>     
+    <!-- end testimonial -->     
+    <!--  footer -->     
+    <footer id="contact"> 
+</footer>     
+    <!-- end footer -->     
+    <!-- Javascript files-->     
+    <script src="assets/js/jquery.min.js"></script>     
+    <script src="assets/js/popper.js"></script>     
+    <script src="js/bootstrap.bundle.min.js"></script>     
+    <script src="js/jquery-3.0.0.min.js"></script>     
+    <script src="js/plugin.js"></script>     
+    <!-- sidebar -->     
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>     
+    <script src="js/custom.js"></script>     
+    <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>     
+    <script src="components/pg.blocks/js/plugins.js"></script>     
+    <script src="components/pg.blocks/js/bskit-scripts.js"></script>     
+    <script src="https://maps.google.com/maps/api/js?sensor=true"></script>     
+    <script src="bootstrap/js/bootstrap.min.js"></script>     
+    <script src="components/pg.blocks.wp/js/plugins.js"></script>
+    <script src="components/pg.blocks.wp/js/bskit-scripts.js"></script>
+</body> 
---
+++ PHP/terms.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html> 
+<html lang="en" wp-site wp-site-is-master-page> 
+    <head> 
+        <!-- basic -->         
+        <meta charset="utf-8"> 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+        <!-- mobile metas -->         
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1"> 
+        <!-- site metas -->         
+        <title>The Yellow Cabs</title>         
+        <meta name="keywords" content=""> 
+        <meta name="description" content=""> 
+        <meta name="author" content=""> 
+        <!-- bootstrap css -->         
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"> 
+        <!-- style css -->         
+        <link rel="stylesheet" href="css/style.css"> 
+        <!-- Responsive-->         
+        <link rel="stylesheet" href="css/responsive.css"> 
+        <!-- fevicon -->         
+        <link rel="icon" href="images/fevicon.png" type="image/gif"/> 
+        <!-- Scrollbar Custom CSS -->         
+        <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css"> 
+        <!-- Tweaks for older IEs-->         
+        <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css"> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen"> 
+        <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->         
+        <link rel="stylesheet" href="components/pg.blocks/css/blocks.css"> 
+        <link rel="stylesheet" href="components/pg.blocks/css/plugins.css"> 
+        <link rel="stylesheet" href="components/pg.blocks/css/style-library-1.css"> 
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700"> 
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic"> 
+        <link rel="stylesheet" href="blocks.css"> 
+        <link rel="stylesheet" href="components/pg.blocks.wp/css/blocks.css"> 
+        <link rel="stylesheet" href="components/pg.blocks.wp/css/plugins.css"> 
+        <link rel="stylesheet" href="components/pg.blocks.wp/css/style-library-1.css"> 
+    </head>     
+    <!-- body -->     
+    <body class="main-layout"> 
+        <!-- loader  -->         
+        <div class="loader_bg"> 
+            <div class="loader"> 
+                <img src="images/loading.gif" alt="#" style="width: 150px;"/> 
+            </div>             
+        </div>         
+        <!-- end loader -->         
+        <!-- header -->         
+        <header> 
+            <!-- header inner -->             
+            <div class="header pt-3"> 
+                <div class="row"> 
+                    <div class="col-6 col-md-4 mr-0 pl-4 pr-0 d-flex flex-wrap align-items-center">
+                        <a href="index.php"><img src="images/logo.png" style="width: 250px;" alt="The Yellow Cabs"></a> 
+                    </div>
+                    <div class="col-md-4 col-6">
+                        <div class="ml-0 mr-0 pl-0 pr-0 sign_btn text-center" style="white-space: nowrap;">
+                            <a href="tel:+919638802666" class="pl-2 pr-2" style="font-size: 125%;"><i class="fa fa-lg fa-phone"></i>+91-9638802666</a>
+                        </div>                         
+                    </div>
+                    <div class="col-md-4">
+                        <div class="d-none d-sm-block pl-0 sign_btn text-right text-sm-right">
+                            <a href="inquirey.php" class="pl-3 pr-3" style="font-size: 120%;">Inquirey</a>
+                        </div>                         
+                    </div>
+                </div>                 
+            </div>             
+        </div>             
+            <section class="bg-light pb-5 pt-5 text-center"> 
+                <div class="container pb-4 pt-4"> 
+                    <div class="align-items-center mt-5 row"> 
+                        <div class="col-lg-11 ml-auto mr-auto text-center"> 
+                            <h2 class="h6 mb-1 text-black-50">Take a Look at</h2> 
+                            <h3 class="font-weight-bold h2 mb-3">Our Terms &amp; Conditions</h3> 
+                            <p><h5 _ngcontent-c5="">Introduction</h5><p _ngcontent-c5="">Welcome to our website. If you continue to browse and use this website, you are agreeing to comply with and be bound by the following terms and conditions of use, which together with our privacy policy govern Savaari&apos;s relationship with you in relation to this website. If you disagree with any part of these terms and conditions, please do not use our website.</p><h5 _ngcontent-c5="">Terms of Use:</h5><p _ngcontent-c5="">The use of this website is subject to the following terms of use:</p><ol _ngcontent-c5=""> 
+                                    <li _ngcontent-c5="" class="text-left">1.The content of the pages of this website is for your general information and use only. It is subject to change without notice.</li>                                     
+                                    <li _ngcontent-c5="" class="text-left">2. This website uses cookies to monitor browsing preferences. If you do allow cookies to be used, the following personal information may be stored by us for use by third parties: IP Address, Location</li>                                     
+                                    <li _ngcontent-c5="" class="text-left">3. Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.</li>                                     
+                                    <li _ngcontent-c5="" class="text-left">4. Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through this website meet your specific requirements.</li>                                     
+                                    <li _ngcontent-c5="" class="text-left">5. This website contains material which is owned by or licensed to us. This material includes, but is not limited to, the design, layout, look, appearance and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these terms and conditions.</li>                                     
+                                    <li _ngcontent-c5="" class="text-left">6. All trademarks reproduced in this website which are not the property of, or licensed to, the operator are acknowledged on the website.</li>                                     
+                                    <li _ngcontent-c5="" class="text-left">7. Unauthorized use of this website may give rise to a claim for damages and/or be a criminal offence.</li>                                     
+                                    <li _ngcontent-c5="" class="text-left">8. From time to time this website may also include links to other websites. These links are provided for your convenience to provide further information. They do not signify that we endorse the website(s). We have no responsibility for the content of the linked website(s).</li>                                     
+                                    <li _ngcontent-c5="" class="text-left">9. Your use of this website and any dispute arising out of such use of the website is subject to the laws of India.</li>                                     
+                                    <li _ngcontent-c5="" class="text-left">10. Specific offers will have might have additional Terms &amp; Conditions which the user has to comply with in case he chooses to avail that offer.</li>                                     
+                                </ol><h5 _ngcontent-c5="">Cancellation and Returns</h5><p _ngcontent-c5="">You may cancel the booking 24 hour prior to the time of journey, without any cancellation charges for all services. In case cancellation or shorting of the trip is requested within 24 hours of the pick-up time, then following rules will apply:</p><ol _ngcontent-c5=""> 
+                                    <li _ngcontent-c5="">Multi Day trip: The charge for the first day would be deducted from the total amount and refund will be issued to the user.</li>                                     
+                                    <li _ngcontent-c5="">Single Day trip/ Airport transfer: No Refund will be issued to the user.</li>                                     
+                                    <li _ngcontent-c5="">Airport transfer: No Cancellation Charges if Cancelled at least 2 hours prior of pickup time.</li>                                     
+                                </ol></p> 
+                        </div>                         
+                    </div>                     
+                </div>                 
+            </section>             
+        </header>         
+        <!-- end header inner -->         
+        <!-- end header -->         
+        <!-- banner -->         
+        <!-- end banner -->         
+        <!-- form_lebal -->         
+        <section> 
+            <div class="container"> 
+                <div class="row"> 
+                    <div class="col-md-12"> 
+</div>                     
+                </div>                 
+            </div>             
+        </section>         
+        <!-- end form_lebal -->         
+        <!-- choose  section -->         
+        <!-- end choose  section -->         
+        <!-- our  section -->         
+        <!-- end our  section -->         
+        <!-- about -->         
+        <!-- end about -->         
+        <!-- testimonial -->         
+        <section>
+            <section class="bg-dark content-block-nopad footer-wrap-1-1">
+                <div class="container footer-1-1">
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-1">
+                            <div class="row">
+                        </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <div class="col-md-4 col-xl-4">
+                            <h4 class="text-white">COMPANY</h4>
+                            <ul>
+                                <li style="border:rgba(0, 0, 0, 0.19);">
+                                    <a href="about.php" class="text-white">● About us</a>
+                                </li>
+                                <li style="border:rgba(0, 0, 0, 0.19);">
+                                    <a href="privacy.php" class="text-white">● Privacy Policy</a>
+                                </li>
+                                <li style="border:rgba(0, 0, 0, 0.19);">
+                                    <a href="terms.php" class="text-white">● Terms &amp; Conditions</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4 col-xl-4">
+                            <h4 class="text-white">SERVICES&nbsp;</h4>
+                            <ul>
+                                <li style="border:rgba(0, 0, 0, 0.19);">
+                                    <a href="index.php" class="text-white">● Local Car Rentals</a>
+                                </li>
+                                <li style="border:rgba(0, 0, 0, 0.19);">
+                                    <a href="index.php" class="text-white">● Outstation Taxi</a>
+                                </li>
+                                <li style="border:rgba(0, 0, 0, 0.19);">
+                                    <a href="attachtaxi.php" class="text-white">● Attach Taxi</a>
+                                </li>
+                                <li style="border:rgba(0, 0, 0, 0.19);">
+                                    <a href="corporate.php" class="text-white">● Corporate Car Rental</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4 col-xl-4">
+                            <h4 class="text-white">GET IN TOUCH&nbsp;</h4>
+                            <ul>
+                                <li style="border:rgba(0, 0, 0, 0.19);">
+                                    <a href="contact.php" class="text-white">● Contact Us</a>
+                                </li>
+                                <li style="border:rgba(0, 0, 0, 0.19);">
+                                    <span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="bg-primary fa fa-facebook-square fa-inverse fa-stack-1x" style="border-radius: 25px;"></i></span>
+                                    <span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="bg-info fa fa-inverse fa-stack-1x fa-twitter-square" style="border-radius: 25px;"></i></span>
+                                    <span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="bg-danger fa fa-instagram fa-inverse fa-stack-1x" style="border-radius: 25px;"></i></span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.container -->
+            </section>
+            <div class="copyright-bar" style="background-color: #FDB813;">
+                <div class="container">
+                    <p class="text-body text-center">Copyright &copy; 2021  All Right Reserved | <a href="" class="text-body">WILTEGIC</a></p>
+                </div>
+            </div>
+        </section>         
+        <!-- end testimonial -->         
+        <!--  footer -->         
+        <footer id="contact"> 
+</footer>         
+        <!-- end footer -->         
+        <!-- Javascript files-->         
+        <script src="assets/js/jquery.min.js"></script>         
+        <script src="assets/js/popper.js"></script>         
+        <script src="js/bootstrap.bundle.min.js"></script>         
+        <script src="js/jquery-3.0.0.min.js"></script>         
+        <script src="js/plugin.js"></script>         
+        <!-- sidebar -->         
+        <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>         
+        <script src="js/custom.js"></script>         
+        <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>         
+        <script src="components/pg.blocks/js/plugins.js"></script>         
+        <script src="components/pg.blocks/js/bskit-scripts.js"></script>         
+        <script src="https://maps.google.com/maps/api/js?sensor=true"></script>         
+        <script src="bootstrap/js/bootstrap.min.js"></script>         
+        <script src="components/pg.blocks.wp/js/plugins.js"></script>         
+        <script src="components/pg.blocks.wp/js/bskit-scripts.js"></script>         
+    </body>     
+</html>
