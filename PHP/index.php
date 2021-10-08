c662bd215041ff80c70a1966eb199d5365a7c1bf
Bhargav Vaghasiya <bhargav.vaghasiya@brainvire.com>
2021-10-08 10:56:23 +0530

    7-10 changes

--- PHP/index.php
+++ PHP/index.php
@@ -42,7 +42,8 @@ $cityArray= get_city_dropdown();
         <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic"> 
         <link rel="stylesheet" href="blocks.css"> 
         <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
-        <style type="text/css">.nav-tabs .nav-link.active { color: #fff; background-color: #ffc107; }</style>         
+        <link rel="stylesheet" href="css/bootstrap-datepicker.css"> 
+        <style type="text/css">.nav-tabs .nav-link.active { color: #fff; background-color: #ffc107; }</style>     
     </head>     
     <!-- body -->     
     <body class="main-layout"> 
@@ -126,11 +127,11 @@ $cityArray= get_city_dropdown();
                                     <div class="col-md-4" style="padding-top: 15px;"> 
                                         <label class="date">TO</label>                                         
                                         <!-- <input class="book_n" type="text" placeholder="Enter Drop City" name="drop_city">  -->
-                                        <select class="book_n" name="drop_city" id="drop_city"><option value="">Enter Drop City</option></select>
+                                        <select class="book_n" name="drop_city" id="drop_city"></select>
                                     </div>                                     
                                     <div class="col-md-3" style="padding-top: 15px;"> 
                                         <label class="date">PICK UP DATE</label>                                         
-                                        <input class="book_n" type="date" placeholder="Set PickUp Date" name="pickup_date"> 
+                                        <input class="book_n" type="text" placeholder="Set PickUp Date" name="pickup_date" id="pickup_date"> 
                                     </div>                                     
                                 </div>                                 
                                 <div class="row"> 
@@ -589,12 +590,18 @@ $cityArray= get_city_dropdown();
     <script src="components/pg.blocks/js/bskit-scripts.js"></script>     
     <script src="https://maps.google.com/maps/api/js?sensor=true"></script>    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script> 
+    <script src="js/bootstrap-datepicker.js"></script>
     <script>
+        $('#pickup_date').datepicker({
+            format: "dd/mm/yyyy",
+            minDate: 0,
+            todayHighlight: true,
+            autoclose: true,
+        });
         $(document).ready(function(){
-            
             var city = <?php echo json_encode($cityArray); ?>;
 
-            var option= '';
+            var option= '<option value="">Enter PickUp City</option>';
             for(var i=0;i<city.length;i++){
                 option +='<option value="'+city[i].id+'">'+city[i].city+', '+city[i].state+'</option>';
             }
@@ -605,14 +612,22 @@ $cityArray= get_city_dropdown();
                 $("#drop_city option").remove();
                 var from_city= $(this).val();
                 if(from_city!=""){
-                    var to_option= '';
-                    to_option +=option;
+                    var to_option= '<option value="">Enter Drop City</option>';
+                    for(var i=0;i<city.length;i++){
+                        if(city[i].id!=from_city){
+                            to_option +='<option value="'+city[i].id+'">'+city[i].city+', '+city[i].state+'</option>';
+                        }
+                        
+                    }
                     $("#drop_city").append(to_option);
-                    $("#drop_city option[value="+from_city+"]").hide();
-                    $("#drop_city").select2();
+                    // $("#drop_city option[value="+from_city+"]").prop("disabled", true);
+                    
                 }
+
+                
                 
             });
+            $("#drop_city").select2();
             $("#pickup_city").select2();
             
             $("input[name='triptype']").click(function() {
